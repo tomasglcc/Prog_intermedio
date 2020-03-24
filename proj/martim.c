319a2290949601213313fd353e4c85ed6014f87c
Tomás C <46073322+tomasglcc@users.noreply.github.com>
2020-03-24 15:41:59 +0000

    Delete martim.c

--- proj/martim.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    char name[20];
-    printf("Quem e o G que faz anos ?\n");
-    scanf("%s", name);
-    printf("%s vai ter muita cona para o resto da vida", name);
-    return 0;
-}
